29a871141fc1bc6839427f27dbe2c452f663a3d0
przemgk <przem_gk@o2.pl>
2019-11-15 23:16:45 +0100

    Install and configure Prettier

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>This is sample text lorem ipsu dolor sens i dont know that write more</p>
         <a
           className="App-link"
           href="https://reactjs.org"
