8220e2ccb67046f3c80782dd3e4edf8cd1439643
przemgk <przem_gk@o2.pl>
2019-11-16 15:20:30 +0100

    Add global styles to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,17 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import GlobalStyles from 'theme/GlobalStyles';
+import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'theme/mainTheme';
+
+addDecorator(story => (
+  <>
+    <Normalize />
+    <GlobalStyles />
+    <ThemeProvider theme={theme}>{story()}</ThemeProvider>
+  </>
+));
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components/', true, /\.stories\.js$/), module);
