7893c9852fbd11dd22e181974452000d124fb80b
przemgk <przem_gk@o2.pl>
2019-12-13 15:22:29 +0100

    Add media queris for ModalTemplate and 404 page

--- src/templates/ModalTemplate.js
+++ src/templates/ModalTemplate.js
@@ -11,12 +11,16 @@ const StyledWrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  width: 35vw;
+  width: 85vw;
   min-height: 50vh;
   padding: 40px;
   border-radius: 6px;
   box-shadow: 0 0 8px -3px ${({ theme }) => theme.shadowColor};
   background-color: ${({ theme }) => theme.elementBgColor};
+
+  @media screen and (min-width: 768px) {
+    width: 35vw;
+  }
 `;
 
 const ModalTemplate = ({ children }) => <StyledWrapper>{children}</StyledWrapper>;
